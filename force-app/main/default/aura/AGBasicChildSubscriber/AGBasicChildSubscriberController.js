0a36fe03d9ba8608d11abaefd6af529ea760743a
Christian Pelayo <pelayochristian.dev@gmail.com>
2022-05-19 17:23:29 +0800

    Add header comments and indentation format

--- force-app/main/default/aura/AGBasicChildSubscriber/AGBasicChildSubscriberController.js
+++ force-app/main/default/aura/AGBasicChildSubscriber/AGBasicChildSubscriberController.js
@@ -1,10 +1,13 @@
 /**
- * Created by christianpelayo on 4/9/22.
- */
+ * @description       : JS Controller of the Child component as a publisher.
+ * @author            : pelayochristian.dev@gmail.com
+ * @last modified on  : 05-19-2022
+ * @last modified by  : pelayochristian.dev@gmail.com
+**/
 
 ({
-    handleComponentEvent : function(cmp, event) {
-        var messageFromEvent = event.getParam("message");
-        cmp.set("v.messageFromEvent", messageFromEvent);
+    handleComponentEvent: function (cmp, event) {
+        var messageFromEvent = event.getParam('message');
+        cmp.set('v.messageFromEvent', messageFromEvent);
     }
 });
--- force-app/main/default/aura/AGBasicParentPublisher/AGBasicParentPublisherController.js
+++ force-app/main/default/aura/AGBasicParentPublisher/AGBasicParentPublisherController.js
@@ -1,12 +1,16 @@
 /**
- * Created by christianpelayo on 4/9/22.
- */
+ * @description       : JS Controller of the Parent component as a publisher.
+ * @author            : pelayochristian.dev@gmail.com
+ * @last modified on  : 05-19-2022
+ * @last modified by  : pelayochristian.dev@gmail.com
+**/
 
 ({
+    // eslint-disable-next-line no-unused-vars
     publishEvent: function (cmp, event) {
-        var messageEvent = $A.get("e.c:AGMessageApplicationEvent");
+        var messageEvent = $A.get('e.c:AGMessageApplicationEvent');
         messageEvent.setParams({
-           "message" : "Message from publisher component"
+            message: 'Message from publisher component'
         });
         messageEvent.fire();
     }
